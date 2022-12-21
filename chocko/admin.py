dea16d4c0e98c50f67bf675fb7f3465bc6147eb8
abdolrahman kiany <94900618+abdoki7788@users.noreply.github.com>
2022-12-21 11:20:37 +0330

    add models to admin site

--- chocko/admin.py
+++ chocko/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Actor, Category, Company, Movie
 
 # Register your models here.
+
+admin.site.register(Actor)
+admin.site.register(Category)
+admin.site.register(Company)
+admin.site.register(Movie)
