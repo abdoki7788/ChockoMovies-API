0bbc523cc9e6d475f03e796c447b4d016f4ce45a
abdolrahman kiany <94900618+abdoki7788@users.noreply.github.com>
2023-01-02 21:22:28 +0330

    create IsAdminOrCreateOnly permission class

--- chocko/permissions.py
+++ chocko/permissions.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 
 class IsAdminOrReadOnly(BasePermission):
     def has_permission(self, request, view):
-        if request.method in SAFE_METHODS or (request.user and request.user.is_superuser):
+        if request.method in SAFE_METHODS or (request.user.is_authenticated and request.user.is_superuser):
             return True
         return False
 
@@ -16,3 +16,9 @@ class IsAuthorOrReadOnly(BasePermission):
             return True
         else:
             return False
+
+class IsAdminOrCreateOnly(BasePermission):
+    def has_permission(self, request, view):
+        if request.method not in SAFE_METHODS or (request.user.is_authenticated and request.user.is_superuser):
+            return True
+        return False
