c2781ef5adaa6a7b3a065c85ae13b9ef2ef85d9f
abdolrahman kiany <94900618+abdoki7788@users.noreply.github.com>
2023-01-18 10:13:03 +0330

    moviedetailserializer no longer needed

--- chocko/serializers.py
+++ chocko/serializers.py
@@ -23,22 +23,6 @@ class MovieListSerializer(serializers.ModelSerializer):
         model = Movie
         fields = ['id', 'title', 'plot', 'release_date', 'genres', 'image', 'imdb_rating']
 
-class MovieDetailSerializer(serializers.Serializer):
-    id = serializers.CharField(max_length=20)
-    title = serializers.CharField(max_length=50)
-    fullTitle = serializers.CharField(max_length=100)
-    releaseDate = serializers.CharField()
-    plot = serializers.CharField()
-    actorList = serializers.ListField()
-    genreList = serializers.ListField()
-    companyList = serializers.ListField()
-    countryList = serializers.ListField()
-    imDbRating = serializers.CharField(max_length=4)
-    image = serializers.URLField()
-    trailer = serializers.URLField()
-    runtimeMins = serializers.CharField()
-    contentRating = serializers.CharField()
-
 class ActorSerializer(serializers.ModelSerializer):
     class Meta:
         model = Actor
